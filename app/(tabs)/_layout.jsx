4d2d901f8aeb11976e14d6cda026c20770c6b95b
Jimmy <nkgogojimmy@gmail.com>
2024-08-14 00:18:32 +0200

    added the safe area view in the home page

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,14 +1,31 @@
 import { Tabs } from "expo-router";
+import { AntDesign, MaterialCommunityIcons } from "@expo/vector-icons";
 
 const TabLayout = () => {
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
+        tabBarActiveTintColor: "blue", // this colour is the one we use when you are on that tab
+        tabBarInactiveTintColor: "grey", // and this is the colour that shows when you are not on that tab
       }}
     >
-      <Tabs.Screen name="home" />
-      <Tabs.Screen name="profile" />
+      <Tabs.Screen
+        name="home"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="home" size={24} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 };
--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,12 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context"; // I use this so that the app can start below the notch since phones have notches these days
 
 const Home = () => {
-
   return (
-    <View>
-      {/* <Text>{name}</Text> */}
-    </View>
+    <SafeAreaView style={{ flex: 1, alignItems: "center" }}>
+      <View style={{ width: "99%", flex: 1 }}>
+        <Text>Home</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
